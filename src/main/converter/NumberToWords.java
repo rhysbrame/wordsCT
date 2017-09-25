250663d08d44552e3d9ff637f407a2fecc3024c8
rhysbrame <rhysbrame@gmail.com>
2017-09-25 15:11:17 +0100

    path setup

--- src/main/converter/NumberToWords.java
+++ src/main/converter/NumberToWords.java
@@ -2,5 +2,10 @@ package main.converter;
 
 public class NumberToWords {
 
-
+    public static String convert(int num) {
+        if (num == 0) {
+            return "zero";
+        }
+        return null;
+    }
 }
